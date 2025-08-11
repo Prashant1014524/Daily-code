2c8790df5385697aea4f2e0de3c4024f0fc80462
prashant <prashantuniyal22@gmail.com>
2025-08-12 01:55:32 +0530

    sss

--- String/String1.cpp
+++ String/String1.cpp
@@ -1,231 +1,287 @@
 
-// // // // // // Char Array are faster than the string 
+// // // // // // // // Char Array are faster than the string 
 
 
 
-// // // // // // #include<iostream>
-// // // // // // using namespace std;
-// // // // // // int main(){
-// // // // // //  char ch[100];
-// // // // // //  cin>>ch;   // it will only read the input stream upto enter ,space ,tab
-// // // // // // //  .getLine() is used to take the inputs in the 
+// // // // // // // // #include<iostream>
+// // // // // // // // using namespace std;
+// // // // // // // // int main(){
+// // // // // // // //  char ch[100];
+// // // // // // // //  cin>>ch;   // it will only read the input stream upto enter ,space ,tab
+// // // // // // // // //  .getLine() is used to take the inputs in the 
 
-// // // // // // cout<<ch;  
-
-
-// // // // // // }
+// // // // // // // // cout<<ch;  
 
 
-// // // // // // getting the length of the char array
-// // // // // #include<iostream>
-// // // // // using namespace std;
+// // // // // // // // }
 
-// // // // // int findLength(char ch[100],int size){
-// // // // //     int length=0;
-// // // // //     for(int i=0;i<size;i++){
+
+// // // // // // // // getting the length of the char array
+// // // // // // // #include<iostream>
+// // // // // // // using namespace std;
+
+// // // // // // // int findLength(char ch[100],int size){
+// // // // // // //     int length=0;
+// // // // // // //     for(int i=0;i<size;i++){
         
-// // // // //         if(ch[i]=='\0'){
-// // // // //             break;
+// // // // // // //         if(ch[i]=='\0'){
+// // // // // // //             break;
 
 
-// // // // //         }
-// // // // //         else{
-// // // // //             length++;
-// // // // //         }
-// // // // //     }
-// // // // //     return length;
-// // // // // }
-// // // // // int main(){
-// // // // //     char ch[100];
-// // // // //     cin>>ch;
-// // // // //     int len=findLength(ch,100);
-// // // // //     cout<<len;
+// // // // // // //         }
+// // // // // // //         else{
+// // // // // // //             length++;
+// // // // // // //         }
+// // // // // // //     }
+// // // // // // //     return length;
+// // // // // // // }
+// // // // // // // int main(){
+// // // // // // //     char ch[100];
+// // // // // // //     cin>>ch;
+// // // // // // //     int len=findLength(ch,100);
+// // // // // // //     cout<<len;
 
-// // // // // }
-
-
-// // // // // Reverse of an String
-// // // // #include<iostream>
-// // // // #include<string.h>
-// // // // using namespace std;
-// // // // // void reverseString(char ch[100],int len){
-// // // // //     int left=0;
-// // // // //     int right=len-1;
-// // // // //     while(left<=right){
-// // // // //         swap(ch[left],ch[right]);
-// // // // //         left++;
-// // // // //         right--;
+// // // // // // // }
 
 
-// // // // //     }
-// // // // //     for(int i=0;i<len;i++){
-// // // // //         cout<<ch[i];
-// // // // //     }
+// // // // // // // Reverse of an String
+// // // // // // #include<iostream>
+// // // // // // #include<string.h>
+// // // // // // using namespace std;
+// // // // // // // void reverseString(char ch[100],int len){
+// // // // // // //     int left=0;
+// // // // // // //     int right=len-1;
+// // // // // // //     while(left<=right){
+// // // // // // //         swap(ch[left],ch[right]);
+// // // // // // //         left++;
+// // // // // // //         right--;
+
+
+// // // // // // //     }
+// // // // // // //     for(int i=0;i<len;i++){
+// // // // // // //         cout<<ch[i];
+// // // // // // //     }
 
     
     
-// // // // // }
+// // // // // // // }
 
 
 
-// // // // // void upperCase(char ch[100],int len){
-// // // // //     int i=0;
-// // // // //     while(ch[i]!='\0'){
-// // // // //         // my string can have lower & upper case values then i must hve check first that the char is in lower case and i should convert it into upper case
-// // // // //         if(ch[i]>='a'&&ch[i]<='z'){
-// // // // //             ch[i]=ch[i]-'a'+'A';
+// // // // // // // void upperCase(char ch[100],int len){
+// // // // // // //     int i=0;
+// // // // // // //     while(ch[i]!='\0'){
+// // // // // // //         // my string can have lower & upper case values then i must hve check first that the char is in lower case and i should convert it into upper case
+// // // // // // //         if(ch[i]>='a'&&ch[i]<='z'){
+// // // // // // //             ch[i]=ch[i]-'a'+'A';
 
 
-// // // // //         }
-// // // // //         else {
-// // // // //             i++;
-// // // // //         }
+// // // // // // //         }
+// // // // // // //         else {
+// // // // // // //             i++;
+// // // // // // //         }
 
 
-// // // // //     }
-// // // // // }
+// // // // // // //     }
+// // // // // // // }
 
 
-// // // // // insert space in the string when @came in the String 
-// // // // // void insertSpace(char ch[100],int len){
-// // // // //     int index=0;
-// // // // //     while(ch[index]!='\0'){
-// // // // //         if(ch[index]=='@'){
-// // // // //             ch[index]=' ';
-// // // // //         }
-// // // // //         index++;
+// // // // // // // insert space in the string when @came in the String 
+// // // // // // // void insertSpace(char ch[100],int len){
+// // // // // // //     int index=0;
+// // // // // // //     while(ch[index]!='\0'){
+// // // // // // //         if(ch[index]=='@'){
+// // // // // // //             ch[index]=' ';
+// // // // // // //         }
+// // // // // // //         index++;
 
-// // // // //     }
-// // // // // }
-// // // // int  checkPalindrome(char ch[100],int len){
-// // // //     int left=0;
-// // // //     int right=len-1;
-// // // //     while(left<=right){
-// // // //         if(ch[left]==ch[right]){
-// // // //             left++;
-// // // //             right--;
+// // // // // // //     }
+// // // // // // // }
+// // // // // // int  checkPalindrome(char ch[100],int len){
+// // // // // //     int left=0;
+// // // // // //     int right=len-1;
+// // // // // //     while(left<=right){
+// // // // // //         if(ch[left]==ch[right]){
+// // // // // //             left++;
+// // // // // //             right--;
 
 
-// // // //         }
-// // // //         else {
-// // // //             return -1;
-// // // //         }
+// // // // // //         }
+// // // // // //         else {
+// // // // // //             return -1;
+// // // // // //         }
     
         
 
         
        
-// // // //     }
-// // // //     return 1;
-// // // // }
+// // // // // //     }
+// // // // // //     return 1;
+// // // // // // }
+// // // // // // int main(){
+// // // // // //     char ch[100];
+// // // // // //     cin.getline(ch,100);
+// // // // // //     int len=strlen(ch);
+// // // // // //     // reverseString(ch,len);
+// // // // // // // cout<<ch;
+// // // // // // // Converting the lower case () to upper case()
+// // // // // // // upper case =lowercase-'a'+'A'
+// // // // // // // upperCase(ch,len);
+// // // // // // // cout<<ch;
+
+// // // // // // // insert space in the string when @came 
+// // // // // // // insertSpace(ch,len);
+// // // // // // // cout<<ch;
+// // // // // // // }
+// // // // // // // Check Palindrome 
+// // // // // // // int ans =checkPalindrome(ch,len);
+// // // // // // // if (ans ==1){
+// // // // // // //     cout<<"palindrome";
+// // // // // // // }
+// // // // // // // else {
+// // // // // // //     cout<<"not palidrome";}
+
+// // // // // // // }
+
+
+// // // // // // strings :- they are dynamic in nature like the vector 
+// // // // // #include<iostream>
+// // // // // using namespace std;
+// // // // // int main(){
+// // // // // // string str;     //you dont have to specify the length /size of the string '
+
+// // // // // //     cin>>str;
+// // // // // //     int i=0;
+// // // // // //     while(str[i] != '\0'){
+// // // // // //         cout<<"The index is"<<i<<" "<<"Element is "<<" "<<str[i]<<endl;
+// // // // // //         i ++;
+
+
+
+// // // // // //     }
+// // // // // //     cout<<str[8];
+
+
+// // // // // // string utilities have the varities of utitlities 
+
+// // // // // // string str1=" Prashant Uniyal ";
+// // // // // // string str2=" kumar ";
+// // // // // // str1.insert(10,str2);
+// // // // // // cout<<str1;
+
+// // // // // string str1="yar tera super star";
+// // // // // string str2="te";
+// // // // // if(str1.find(str2)==string::npos){
+// // // // //     cout<<"Not found";
+// // // // // }
+// // // // // else{
+// // // // //     cout<<"found";
+// // // // // }
+
+
+
+// // // // // }
+// // // // #include<iostream>
+// // // // #include<string.h>
+// // // // using namespace std;
 // // // // int main(){
-// // // //     char ch[100];
-// // // //     cin.getline(ch,100);
-// // // //     int len=strlen(ch);
-// // // //     // reverseString(ch,len);
-// // // // // cout<<ch;
-// // // // // Converting the lower case () to upper case()
-// // // // // upper case =lowercase-'a'+'A'
-// // // // // upperCase(ch,len);
-// // // // // cout<<ch;
+// // // //     int k=8;
+    
+// // // //     string str="hellloprashant";
 
-// // // // // insert space in the string when @came 
-// // // // // insertSpace(ch,len);
-// // // // // cout<<ch;
-// // // // // }
-// // // // // Check Palindrome 
-// // // // // int ans =checkPalindrome(ch,len);
-// // // // // if (ans ==1){
-// // // // //     cout<<"palindrome";
-// // // // // }
-// // // // // else {
-// // // // //     cout<<"not palidrome";}
-
-// // // // // }
+// // // //     str.erase(str.end()-k,str.end());
+// // // //     cout<<str<<endl;
+// // // //     cout<<str.back();
 
 
-// // // // strings :- they are dynamic in nature like the vector 
+// // // // }
+
+
+// // // // Remove  the dupliacte from the string 
+
+
 // // // #include<iostream>
 // // // using namespace std;
-// // // int main(){
-// // // // string str;     //you dont have to specify the length /size of the string '
-
-// // // //     cin>>str;
-// // // //     int i=0;
-// // // //     while(str[i] != '\0'){
-// // // //         cout<<"The index is"<<i<<" "<<"Element is "<<" "<<str[i]<<endl;
-// // // //         i ++;
-
-
-
+// // // // void removeD(string str,int size){
+// // // //     int index=0;
+// // // //     string ans="";
+// // // //     while(index<str.length()){
+// // // //         if(ans.length()>0&&ans[ans.length()-1]==str[index]){
+// // // //             ans.pop_back();
+// // // //         }
+// // // //         else{
+// // // //             ans.push_back(str[index]);
+// // // //         }
+// // // //     index++;
+    
 // // // //     }
-// // // //     cout<<str[8];
 
+// // // // cout<<ans ;
+// // // // }
+// // // int main(){
+// // //     // string str="abbaca";
+// // //     // int size=str.length();
+// // //     // removeD(str,size);
 
-// // // // string utilities have the varities of utitlities 
-
-// // // // string str1=" Prashant Uniyal ";
-// // // // string str2=" kumar ";
-// // // // str1.insert(10,str2);
-// // // // cout<<str1;
-
-// // // string str1="yar tera super star";
-// // // string str2="te";
-// // // if(str1.find(str2)==string::npos){
-// // //     cout<<"Not found";
-// // // }
-// // // else{
-// // //     cout<<"found";
+// // //     string s="abccab";
+// // //     string s2="bcc";
+// // //    cout<< s.find(s2);
 // // // }
 
-
-
-// // // }
+// // // Palindrome substrings 
 // // #include<iostream>
 // // #include<string.h>
 // // using namespace std;
-// // int main(){
-// //     int k=8;
-    
-// //     string str="hellloprashant";
 
-// //     str.erase(str.end()-k,str.end());
-// //     cout<<str<<endl;
-// //     cout<<str.back();
+// // int expand(string s,int i,int j){
+// //     int count=0;
+// //     while(i>=0&&j<s.length()&&s[i]==s[j]){
+// //         i--;
+// //         j++;
+// //         count++;
+
+// //     }
+// //     return count;
+
+// // }
+// // int main(){
+// //     string s="aaa";
+// //     int totalCount=0;
+// //     for(int center=0;center<s.length();center++){
+// //         // for odd palindrome checking 
+// //         int oddAns=expand(s,center,center);
+// //         int evenAns=expand(s,center,center+1);
+// //         totalCount=totalCount+oddAns+evenAns;
+// //     }
+// //     cout<< totalCount;
 
 
 // // }
 
 
-// // Remove  the dupliacte from the string 
+
+  
 
 
 // #include<iostream>
 // using namespace std;
-// // void removeD(string str,int size){
-// //     int index=0;
-// //     string ans="";
-// //     while(index<str.length()){
-// //         if(ans.length()>0&&ans[ans.length()-1]==str[index]){
-// //             ans.pop_back();
-// //         }
-// //         else{
-// //             ans.push_back(str[index]);
-// //         }
-// //     index++;
-    
-// //     }
-
-// // cout<<ans ;
-// // }
 // int main(){
-//     // string str="abbaca";
-//     // int size=str.length();
-//     // removeD(str,size);
+//     int arr[]={10,20,30};
+//     int size=3;
 
-//     string s="abccab";
-//     string s2="bcc";
-//    cout<< s.find(s2);
+// //     int arr2[size];
+
+// //   for(int i=0;i<size;i++){
+// //     arr2[i]=arr[i];
+
+
+// //   }
+// //   arr2[0]=10;
+//   for(int i=0;i<size;i++){
+//     cout<<arr[i]<<" ";
+//   }
+
+
+
+ 
 // }
-
-
